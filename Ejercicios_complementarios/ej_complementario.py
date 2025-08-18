abdb7ecc690cb060cf7bdcc41555d9a51f625b55
Agustina Fontagnol <agusfontagnol8@gmail.com>
2025-08-18 19:20:59 -0300

    EJERCICIOS COMPLEMENTARIOS

---
+++ Ejercicios_complementarios/ej_complementario.py
@@ -0,0 +1,67 @@
+print("----- EJERCICIOS COMPLEMENTARIOS ----")
+# PUNTO 1
+numero1 = 17
+
+# PUNTO 2
+numero2 = 24.5
+
+# PUNTO 3 y 4
+suma = numero1 + numero2
+print(f"Suma: {suma}")
+
+resta = numero1 - numero2
+print(f"Resta: {resta}")
+
+multiplicacion = numero1 * numero2
+print(f"Multiplicacion: {multiplicacion}")
+
+division = numero1 / numero2
+print(f"Division: {division}")
+
+#PUNTO 5
+nombre = "Agustina"
+
+#PUNTO 6
+precio = 36500.50
+
+#PUNTO 7 
+descuento = 0.50
+
+#PUNTO 8
+precio_final = precio * descuento
+
+#PUNTO 9
+cadena = "Estoy tomando mate"
+
+#PUNTO 10
+longitud = len(cadena)
+
+#PUNTO 11
+precio = 30.9
+precio = int(precio)
+
+#PUNT0 12
+nombre = "Agustina"
+apellido = "Fontagnol"
+
+nombre_completo = nombre + " " + apellido
+
+#PUNTO 13
+mi_edad = 18
+edad_incrementa = mi_edad + 5
+edad_disminuye = edad_incrementa - 10
+
+#PUNTO 14
+
+altura = 1.64
+altura = (altura * 4)
+altura = (altura / 3)
+
+#PUNTO 15
+nombre_mayus = "AGUSTINA FONTAGNOL"
+nombre_minus = nombre_mayus.lower()
+
+#PUNTO 16
+
+nombre_mayus = nombre_mayus.capitalize()
+print(nombre_mayus)
