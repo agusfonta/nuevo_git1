9a6e20cecf37f84f91d3b6cca3afe9491f7197b0
Agustina Fontagnol <agusfontagnol8@gmail.com>
2025-08-18 19:35:41 -0300

    Ejercicios complementarios terminado

--- Ejercicios_complementarios/ej_complementario.py
+++ Ejercicios_complementarios/ej_complementario.py
@@ -1,11 +1,28 @@
-print("----- EJERCICIOS COMPLEMENTARIOS ----")
+print("")
+print("______EJERCICIOS COMPLEMENTARIOS____")
+print("")
+
 # PUNTO 1
+print("")
+print("....PUNTO 1.....")
+print("")
+
 numero1 = 17
+print(f"Numero entero: {numero1}")
 
 # PUNTO 2
+print("")
+print("....PUNTO 2.....")
+print("")
+
 numero2 = 24.5
+print(f"Numero decimal: {numero2}")
 
 # PUNTO 3 y 4
+print("")
+print("....PUNTO 3 Y 4 .....")
+print("")
+
 suma = numero1 + numero2
 print(f"Suma: {suma}")
 
@@ -19,49 +36,99 @@ division = numero1 / numero2
 print(f"Division: {division}")
 
 #PUNTO 5
+print("")
+print("....PUNTO 5.....")
+print("")
+
 nombre = "Agustina"
+print(f"Nombre: {nombre}")
 
-#PUNTO 6
+#PUNTO 6, 7 y 8
+print("")
+print("....PUNTO 6, 7 y 8.....")
+print("")
+
 precio = 36500.50
+print(f"Precio: {precio}")
 
-#PUNTO 7 
 descuento = 0.50
+print(f"Descuento: {descuento}")
 
-#PUNTO 8
 precio_final = precio * descuento
+print(f"Precio final: {precio_final}")
 
-#PUNTO 9
+#PUNTO 9 y 10
+print("")
+print("....PUNTO 9 y 10.....")
+print("")
+
 cadena = "Estoy tomando mate"
+print(f"Frase: {cadena}")
 
-#PUNTO 10
+
 longitud = len(cadena)
+print(f"Longitud: {longitud}")
 
 #PUNTO 11
+print("")
+print("....PUNTO 11....")
+print("")
+
 precio = 30.9
+print(f"Precio decimal: {precio}")
 precio = int(precio)
+print(f"Precio entero: {precio}")
 
 #PUNT0 12
+print("")
+print("....PUNTO 12.....")
+print("")
+
 nombre = "Agustina"
+print(f"Nombre: {nombre}")
 apellido = "Fontagnol"
+print(f"Apellido: {apellido}")
 
 nombre_completo = nombre + " " + apellido
+print(f"Nombre completo: {nombre_completo}")
+
 
 #PUNTO 13
+print("")
+print("....PUNTO 13.....")
+print("")
+
 mi_edad = 18
+print(f"Edad: {mi_edad}")
+
 edad_incrementa = mi_edad + 5
-edad_disminuye = edad_incrementa - 10
+edad_disminuye = mi_edad - 10
+print(f"Mi edad incrementandole 5 es {edad_incrementa} y disminuyendole 10 es: {edad_disminuye} ")
 
 #PUNTO 14
+print("")
+print("....PUNTO 14.....")
+print("")
 
 altura = 1.64
-altura = (altura * 4)
-altura = (altura / 3)
+print(f"Altura: {altura}")
 
-#PUNTO 15
+altura_multiplicada4= float(altura) * 4
+print(f" Mi altura multiplicada por 4: {altura_multiplicada4}")
+
+altura_dividida3= float(altura) / 3
+print(f" Mi altura dividida por 3: {altura_dividida3}")
+
+#PUNTO 15 y 16
+print("")
+print("....PUNTO 15 y 16.....")
+print("")
+
 nombre_mayus = "AGUSTINA FONTAGNOL"
 nombre_minus = nombre_mayus.lower()
 
-#PUNTO 16
+print(f"Nombre en mayuscula: {nombre_mayus}")
+print(f"Nombre en minuscula: {nombre_minus}")
 
 nombre_mayus = nombre_mayus.capitalize()
-print(nombre_mayus)
+print(f"Solo primer letra en mayuscula: {nombre_mayus}")
