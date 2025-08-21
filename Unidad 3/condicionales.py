1aede482d378739e7f42caf23534a9deb3e8d310
Agustina Fontagnol <agusfontagnol8@gmail.com>
2025-08-21 12:32:08 -0300

    Unidad 3 Condicionales EJ Terminados

--- Unidad 3/condicionales.py
+++ Unidad 3/condicionales.py
@@ -1,11 +1,18 @@
 
-#___________ PUNTO 7 ______________
-print("______Punto 7 ______")
 
-palabra = input("Ingresa una palabra: ")
-ultima_letra = palabra[len(palabra) - 1]
+#___________ PUNTO 8 ______________
+print("______Punto 8 ______")
 
-if ultima_letra == "a" or ultima_letra == "e" or ultima_letra == "i" or ultima_letra == "o" or ultima_letra == "u":
-    print(f"{palabra}!")
+nombre = input("Ingrese su nombre: ")
+mayus = nombre.upper()
+minus = nombre.lower()
+titulo = nombre.title()
+
+opcion = input(f"Elija 1, 2 o 3 segun lo deseado: \n 1. En mayusculas \n 2. En minusculas \n 3. Primera mayuscula, el resto minuscula \n - ")
+
+if opcion == "1":
+    print(mayus)
+elif opcion == "2":
+    print(minus)
 else:
-    print(f"{palabra}")
+    print(titulo)
